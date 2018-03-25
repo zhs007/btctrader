b20253e0d1bb1cc2eaf3d536da2cf28d042d9612
zhs007 <sssxueren@gmail.com>
2018-03-25 16:53:38 +0800

    upd

---
+++ bin/bithumb_tick_eos.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const bithumb = require('../src/market/bithumb/index');
+
+const fs = require('fs');
+
+const SIMTRADE = true;
+
+const cfg = JSON.parse(fs.readFileSync('./config.json', 'utf-8'));
+
+bithumb.DataMgr.singleton.init(cfg).then(() => {
+    var ds = new bithumb.DataStream({
+        output_message: false,
+        simtrade: SIMTRADE,
+        tickdatatname: 'bithumb_eos',
+        candledatatname: 'bithumb_kl_eos',
+        symbol: 'eos'
+    });
+
+    ds.mgrData = bithumb.DataMgr.singleton;
+    ds.init();
+});
